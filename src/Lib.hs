b25aca4ceab769a94721c78c9a0c9ca0c43f4b6a
Andriy F <1040440+andriy-f@users.noreply.github.com>
2019-05-03 12:51:38 +0300

    remove old code

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,16 +23,4 @@ muln :: Num a => a -> [a] -> [a]
 muln n arr = foldr (\x acc -> x * n : acc) [] arr
 
 someFunc :: IO ()
-someFunc =
-    putStrLn
-        $  "myAdd: 3 + 4 = "
-        ++ show (myAdd 3 4)
-        ++ "\n"
-        ++ let arr2sort = [10, 2, 5, 3, 1, 6, 7, 4, 2, 3, 4, 8, 9]
-           in  "Qsort "
-               ++ show arr2sort
-               ++ ": "
-               ++ show (qsort arr2sort)
-               ++ "\n"
-               ++ "infinifold"
-               ++ show (take 12 $ muln 2 [1 ..])
+someFunc = putStrLn $ "infinifold" ++ show (take 12 $ muln 2 [1 ..])
