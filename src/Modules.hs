c96332e683d28025db0c343febd08935366464b7
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-05 19:04:46 +0000

    Use Data.Map in code

--- src/Modules.hs
+++ src/Modules.hs
@@ -1,5 +1,7 @@
 module Modules where
 
+import qualified Data.Map as Map
+
 findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey key [] = Nothing
 findKey key ((k, v) : xs) =
@@ -10,3 +12,5 @@ findKey key ((k, v) : xs) =
 -- This is a textbook recursive function that operates on a list
 findKey2 :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey2 key = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing
+
+mapMember = Map.member 3 $ Map.fromList [(3,6),(4,3),(6,9)]
--- test/ModulesSpec.hs
+++ test/ModulesSpec.hs
@@ -1,6 +1,6 @@
 module ModulesSpec where
 
-import Modules (findKey, findKey2)
+import Modules (findKey, findKey2, mapMember)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
@@ -14,3 +14,6 @@ spec = do
     let phoneBook = [("betty", "555-2938"), ("bonnie", "452-2928"), ("patsy", "493-2928"), ("lucille", "205-2928"), ("wendy", "939-8282"), ("penny", "853-2492")]
     it "Nothing if inexistent key" $ findKey2 "no-such-thing" phoneBook `shouldBe` Nothing
     it "Value if key exists" $ findKey2 "patsy" phoneBook `shouldBe` Just "493-2928"
+
+  describe "mapMember" $ do
+    it "mapMember1" $ mapMember `shouldBe` True
