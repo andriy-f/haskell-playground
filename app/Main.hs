054859ca156df60c60abff7415d63613c71a8e81
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-25 13:13:23 +0000

    main func update

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
 module Main where
 
-import PlayTypes (MyVector2D, Vector3D(..), vPlus)
+import PlayTypes (Vector3D (..), vPlus)
 
 main :: IO ()
 main =
-  let
-    -- v1 = MyVector2D 1 2
-    v1 = Vector3D 1 2 3
-    v2 = Vector3D 3 2 1
-  in
-    -- putStrLn "This is Haskell playground.\nTo run tests, enter `stack test`" ++
-    putStrLn ("Vector plus" ++ show (vPlus v1 v2))
+  let -- v1 = MyVector2D 1 2
+      v1 = Vector3D 1 2 3
+      v2 = Vector3D 3 2 1
+   in -- putStrLn "This is Haskell playground.\nTo run tests, enter `stack test`" ++
+      putStrLn ("Vector plus" ++ show (vPlus v1 v2))
