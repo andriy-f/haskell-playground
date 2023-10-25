a32cf1396caaac6d80b7745780a7b91ed28cb014
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-25 12:23:29 +0000

    fix import-export issue with modules

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import PlayTypes (MyVector2D, Vector3D(..), vPlus)
+
 main :: IO ()
-main = putStrLn "This is Haskell playground.\nTo run tests, enter `stack test`"
+main =
+  let
+    -- v1 = MyVector2D 1 2
+    v1 = Vector3D 1 2 3
+    v2 = Vector3D 3 2 1
+  in
+    -- putStrLn "This is Haskell playground.\nTo run tests, enter `stack test`" ++
+    putStrLn ("Vector plus" ++ show (vPlus v1 v2))
--- src/PlayTypes.hs
+++ src/PlayTypes.hs
@@ -1,5 +1,7 @@
 module PlayTypes
-  ( MyVector2D,
+  ( MyVector2D (..),
+    Vector3D (..),
+    vPlus,
     AnimalL,
     animalLName,
     animalLDefault,
@@ -10,10 +12,18 @@ module PlayTypes
   )
 where
 
-data MyVector2D = MyVector2D Int Int
+data MyVector2D = MyVector2D Int Int deriving (Show)
+
+-- Can be Int, Integer, Double (change name)
+data Vector3D a = Vector3D a a a deriving (Show)
+
+vPlus :: (Num a) => Vector3D a -> Vector3D a -> Vector3D a
+(Vector3D x1 y1 z1) `vPlus` (Vector3D x2 y2 z2) = Vector3D (x1+x2) (y1+y2) (z1+z2)
+-- vPlus (Vector3D x1 y1 z1) (Vector3D x2 y2 z2) = Vector3D (x1 + x2) (y1 + y2) (z1 + z2)
 
 data AnimalL = AnimalL String Int Int deriving (Show)
 
+animalLName :: AnimalL -> String
 animalLName (AnimalL name _ _) = name
 
 data AnimalR = AnimalR
@@ -28,16 +38,27 @@ data MyEqVal = True | False
 data Suit = Club | Diamond | Heart | Spade
   deriving (Read, Show, Enum, Eq, Ord)
 
-data CardValue = Two | Three | Four
-     | Five | Six | Seven | Eight | Nine | Ten
-     | Jack | Queen | King | Ace
-  deriving (Read,  Show, Enum, Eq, Ord)
+data CardValue
+  = Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
+  deriving (Read, Show, Enum, Eq, Ord)
 
 animalLDefault :: String -> AnimalL
 animalLDefault name = AnimalL name 4 1
 
 animalRDefault :: String -> AnimalR
-animalRDefault nameVal = AnimalR {name=nameVal, numOfLegs=4, numOfTails=1}
+animalRDefault nameVal = AnimalR {name = nameVal, numOfLegs = 4, numOfTails = 1}
 
 animalRName :: AnimalR -> String
 animalRName = name
