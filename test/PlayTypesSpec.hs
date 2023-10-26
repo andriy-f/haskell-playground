51aa96239d4f758a0efafcaf5ad3e3207e5cd13e
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-26 16:46:40 +0000

    improve tests for types

--- test/PlayTypesSpec.hs
+++ test/PlayTypesSpec.hs
@@ -1,15 +1,15 @@
 module PlayTypesSpec where
 
-import PlayTypes (AnimalL, AnimalR, MyVector2D(..), getVector2Dx, getVector2Dy, animalLDefault, animalLName, animalRDefault, animalRName)
+import PlayTypes (AnimalL (..), AnimalR (..), MyVector2D (..), animalLDefault, animalLName, animalRDefault, animalRName, getVector2Dx, getVector2Dy)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
   describe "AnimalL" $ do
-    it "AnimalL assign" $ let an0 = animalLDefault "Beaver" in animalLName an0 `shouldBe` "Beaver"
+    it "AnimalL assign" $ let an0 = AnimalL "Beaver" 4 1 in animalLName an0 `shouldBe` "Beaver"
 
   describe "AnimalR" $ do
-    it "assign" $ let an0 = animalRDefault "Beaver" in animalRName an0 `shouldBe` "Beaver"
+    it "assign" $ let an0 = AnimalR {name = "Bear", numOfLegs = 4, numOfTails = 1} in animalRName an0 `shouldBe` "Bear"
 
   describe "MyVector2D" $ do
     it "assign and get x" $ let v1 = MyVector2D 1 2 in getVector2Dx v1 `shouldBe` 1
