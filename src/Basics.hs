4c4936603167f570b07d96485b667eba09306d56
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-26 18:17:46 +0000

    Basics: guard function

--- src/Basics.hs
+++ src/Basics.hs
@@ -1,5 +1,12 @@
 module Basics where
 
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+
 mul2 :: Num a => a -> a
 mul2 a = a * 2
 
