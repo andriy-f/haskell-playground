729d0263c07303109e97d890e436ab5ac251f8a7
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-27 17:10:32 +0000

    YesNo TypeClass

--- src/MyTypeClasses.hs
+++ src/MyTypeClasses.hs
@@ -1,6 +1,8 @@
 -- define module
 module MyTypeClasses where
 
+import BinaryTreeModule (BinaryTree (..))
+
 class MyEq a where
   (==) :: a -> a -> Bool
   (/=) :: a -> a -> Bool
@@ -19,3 +21,34 @@ instance Show TrafficLight where
   show Red = "Red light"
   show Yellow = "Yellow light"
   show Green = "Green light"
+
+-- YesNo
+class YesNo a where
+  yesNo :: a -> Bool
+
+instance YesNo Int where
+  yesNo 0 = False
+  yesNo _ = True
+
+instance YesNo [a] where
+  yesNo [] = False
+  yesNo _ = True
+
+instance YesNo Bool where
+  yesNo = id
+
+instance YesNo (Maybe a) where
+  yesNo (Just _) = True
+  yesNo Nothing = False
+
+instance YesNo (BinaryTree a) where
+  yesNo EmptyTree = False
+  yesNo _ = True
+
+instance YesNo TrafficLight where
+  yesNo Red = False
+  yesNo Yellow = False
+  yesNo _ = True
+
+yesNoIf :: (YesNo y) => y -> a -> a -> a
+yesNoIf yesNoVal yesResult noResult = if yesNo yesNoVal then yesResult else noResult
