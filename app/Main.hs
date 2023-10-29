09726415ee7f599a41902681702a333608f830f2
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2023-10-29 15:45:11 +0000

    IO: read and add two 3D vectors

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,16 @@ module Main where
 import PlayTypes (Vector3D (..), vPlus)
 
 main :: IO ()
-main =
-  let -- v1 = MyVector2D 1 2
-      v1 = Vector3D 1 2 3
-      v2 = Vector3D 3 2 1
-   in -- putStrLn "This is Haskell playground.\nTo run tests, enter `stack test`" ++
-      putStrLn ("Vector plus" ++ show (vPlus v1 v2))
+main = do
+  putStrLn "Enter 3 Numbers for 3D Vector #1:"
+  x1 <- getLine
+  y1 <- getLine
+  z1 <- getLine
+  putStrLn "Enter 3 Numbers for 3D Vector #2:"
+  x2 <- getLine
+  y2 <- getLine
+  z2 <- getLine
+  putStrLn "Result of adding those vectors:"
+  let v1 = Vector3D (read x1) (read y1) (read z1)
+      v2 = Vector3D (read x2) (read y2) (read z2)
+   in putStrLn ("Vector plus" ++ show (vPlus v1 v2))
