d5f886dcfbc13e6d32b6653ab0fcd2b53ffc63c5
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-03 19:37:39 +0000

    Play with randoms

--- src/Randomization.hs
+++ src/Randomization.hs
@@ -1,17 +1,31 @@
 module Randomization (main) where
 
-import System.Random (Random, RandomGen, mkStdGen, random)
+import System.Random (Random, RandomGen, mkStdGen, getStdGen,
+  random, randoms, randomR, randomRs)
 
 main = do
   putStrLn "Welcome to randomization app"
-  putStrLn "Nter number of randoms to generate:"
+  putStrLn "Enter number of randoms to generate:"
   numberStr <- getLine
   let number = read numberStr :: Int
-      myRandomMmers = take number (myRandoms (mkStdGen 42) :: [Int])
+      myRandomNumbers = take number (getRandoms (mkStdGen 42) :: [Int])
 
-  putStrLn ("Your randoms: " ++ show myRandomMmers)
+  putStrLn ("Your randoms: " ++ show myRandomNumbers)
 
-myRandoms :: (Random a, RandomGen g) => g -> [a]
-myRandoms g =
+-- get infinite list of randoms [a] from a seed g
+getRandoms :: (Random a, RandomGen g) => g -> [a]
+getRandoms g =
   let (r, gNew) = random g
-   in r : myRandoms gNew
+   in r : getRandoms gNew
+
+getFiniteRandoms :: (Random a, RandomGen g) => Int -> g -> ([a], g)
+getFiniteRandoms 0 g = ([], g)
+getFiniteRandoms n g =
+  let (r, gNew) = random g
+      (randomsNminus1, gFinal) = getFiniteRandoms (n - 1) gNew
+   in (r : randomsNminus1, gFinal)
+
+generatePassword :: Int -> String
+generatePassword n =
+  let (randoms, _) = getFiniteRandoms n (mkStdGen 42)
+   in map toEnum randoms
