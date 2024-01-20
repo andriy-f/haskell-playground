8ff46df01a5ce93064e9db13beb3deff5c937656
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-20 14:42:26 +0000

    example Monad use

---
+++ src/RopeWalking.hs
@@ -0,0 +1,30 @@
+module RopeWalking where
+
+type Birds = Int
+
+type Pole = (Birds, Birds)
+
+landLeftV0 :: Birds -> Pole -> Pole
+landLeftV0 n (left, right) = (left + n, right)
+
+landRightV0 :: Birds -> Pole -> Pole
+landRightV0 n (left, right) = (left, right + n)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) <= 3 = Just (left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) <= 3 = Just (left, right + n)
+  | otherwise = Nothing
+
+-- >>= prototype
+x -: f = f x
+
+exampleBirdsflyV0 :: Pole
+exampleBirdsflyV0 = (0, 0) -: landLeftV0 1 -: landRightV0 1 -: landLeftV0 2
+
+exampleBirdfly :: Maybe Pole
+exampleBirdfly = return (0, 0) >>= landLeft 1 >>= landRight 4 >>= landLeft (-1) >>= landRight (-2)
