9c75eddc13a4b647242182d2bb6d85b60010e672
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-26 19:07:54 +0000

    added different ways to implement listOfTuples (monad sample)

--- src/Monadism.hs
+++ src/Monadism.hs
@@ -5,3 +5,9 @@ listOfTuples = do
   n <- [1, 2]
   ch <- ['a', 'b']
   return (n, ch)
+
+listOfTuples'' :: [(Int, Char)]
+listOfTuples'' = [(n, ch) | n <- [1, 2], ch <- ['a', 'b']]
+
+listOfTuples' :: [(Int, Char)]
+listOfTuples' = [1, 2] >>= (\n -> ['a', 'b'] >>= (\c -> return (n, c)))
