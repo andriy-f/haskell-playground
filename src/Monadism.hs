64254c1e714d4fb7c9714d8cde4e18f4662ccfa6
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-26 20:08:14 +0000

    some more monadism

--- src/Monadism.hs
+++ src/Monadism.hs
@@ -1,5 +1,7 @@
 module Monadism where
 
+import Control.Monad (MonadPlus (mzero))
+
 listOfTuples :: [(Int, Char)]
 listOfTuples = do
   n <- [1, 2]
@@ -11,3 +13,18 @@ listOfTuples'' = [(n, ch) | n <- [1, 2], ch <- ['a', 'b']]
 
 listOfTuples' :: [(Int, Char)]
 listOfTuples' = [1, 2] >>= (\n -> ['a', 'b'] >>= (\c -> return (n, c)))
+
+-- MondaPlus is a typeclass for monads that can also act as monoids
+-- mzero is the identity element of the monoid
+guard :: (MonadPlus m) => Bool -> m ()
+guard True = return ()
+guard False = mzero
+
+filterNumbersContaining7 :: [Int] -> [Int]
+filterNumbersContaining7 xs = xs >>= (\x -> guard ('7' `elem` show x) >> return x)
+
+filterNumbersContaining7' :: [Int] -> [Int]
+filterNumbersContaining7' xs = do
+  x <- xs
+  guard ('7' `elem` show x)
+  return x
