7e65de034064df21d8eb1f20c6235054f3820c40
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-31 11:35:16 +0000

    addDrink usage

---
+++ src/MonadLogs.hs
@@ -0,0 +1,31 @@
+module MonadLogs (applyLog, applyLog2, applyLog3, addDrink) where
+
+import Data.Monoid (Sum)
+
+-- basic appllyLog
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
+
+-- more generic applyLog
+applyLog2 :: (a, [c]) -> (a -> (b, [c])) -> (b, [c])
+applyLog2 (x, log) f = let (y, newLog) = f x in (y, log ++ newLog)
+
+-- Monoidal applyLog
+applyLog3 :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog3 (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+type Food = String
+
+type Price = Sum Int
+
+-- adds milk to beans
+-- adds rice milk to oats
+-- adds borjomi to everything else
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", 25)
+addDrink "oats" = ("rice milk", 99)
+addDrink _ = ("borjomi", 30)
+
+-- addDrink ad
+addDrink2beans :: (Food, Price)
+addDrink2beans = ("beans", 10) `applyLog3` addDrink
