31704306c07090653218bd6d2b86a77877c5e95b
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-06 18:59:25 +0000

    GCD with Monad Log

--- src/MyMath.hs
+++ src/MyMath.hs
@@ -1,4 +1,6 @@
-module MyMath (myGCD) where
+module MyMath (myGCD, myGCDWithLog, myGCDWithMonadicLog) where
+
+import Control.Monad.Writer (Writer, tell)
 
 myGCD :: Int -> Int -> Int
 myGCD a 0 = a
@@ -11,3 +13,11 @@ myGCDWithLog a b = (gcd, log2)
     (gcd, log) = myGCDWithLog b (a `mod` b)
     newLog = "GCD " ++ show a ++ " " ++ show b ++ " = " ++ show gcd
     log2 = newLog : log
+
+myGCDWithMonadicLog :: Int -> Int -> Writer [String] Int
+myGCDWithMonadicLog a 0 = do
+  tell ["Finished with " ++ show a]
+  return a
+myGCDWithMonadicLog a b = do
+  tell ["GCD " ++ show a ++ " " ++ show b]
+  myGCDWithMonadicLog b (a `mod` b)
