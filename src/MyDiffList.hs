efb5a8db6ae53f42333d9693b1c842c77418fd1c
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-13 15:11:42 +0000

    added functions for comparison if using DiffList and not using it

--- src/MyDiffList.hs
+++ src/MyDiffList.hs
@@ -1,5 +1,15 @@
-module MyDiffList (MyDiffList, toMyDiffList, fromMyDiffList) where
+module MyDiffList
+  ( MyDiffList,
+    toMyDiffList,
+    fromMyDiffList,
+    countWithDiffList,
+    countWithoutDiffList,
+    runCountWithDiffList,
+    runCountWithoutDiffList,
+  )
+where
 
+import Control.Monad.Writer (Writer, runWriter, tell)
 import Data.Monoid (Monoid (mappend, mempty))
 
 -- DiffList is analogy of List but with always efficient appending
@@ -17,3 +27,23 @@ instance Semigroup (MyDiffList a) where
 
 instance Monoid (MyDiffList a) where
   mempty = MyDiffList ([] ++)
+
+countWithoutDiffList :: Int -> Writer ([String]) ()
+countWithoutDiffList 0 = do
+  tell ["0"]
+countWithoutDiffList n = do
+  countWithoutDiffList (n - 1)
+  tell [show n]
+
+-- For performance showcase
+runCountWithoutDiffList = mapM_ putStrLn . snd . runWriter $ countWithoutDiffList 500000
+
+countWithDiffList :: Int -> Writer (MyDiffList String) ()
+countWithDiffList 0 = do
+  tell $ toMyDiffList ["0"]
+countWithDiffList n = do
+  countWithDiffList (n - 1)
+  tell $ toMyDiffList [show (n - 1)]
+
+-- For performance showcase
+runCountWithDiffList = mapM_ putStrLn . fromMyDiffList . snd . runWriter $ countWithDiffList 500000
